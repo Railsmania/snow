d60120d4ab7c2b685e29491393f825367f28ad49
Jordi Polo <mumismo@gmail.com>
2014-12-14 18:24:33 +0100

    bugfixing

--- lib/snow/lesson_status.rb
+++ lib/snow/lesson_status.rb
@@ -1,5 +1,6 @@
 class LessonStatus
 
+  #Each language has a different workflow
   def lesson_status(name, language)
     if language == :en
       lesson_status_en(name)
@@ -18,35 +19,38 @@ class LessonStatus
   def next_action(stages)
     option = false
     stages.each_pair do |next_step, condition|
-      if condition.call == false # if we have an optional step return that intead
+      result = condition.call # the call may be expensive
+      if result == false # if we have an optional step return that intead
         return option || next_step
-      elsif condition.call == :option #if fail in optional step, just record it
+      elsif result == :option #if fail in optional step, just record it
         option = option || next_step
       else # if passed then delete options
         option = false
       end
     end
-    stages.last.key
+    'DONE'
   end
 
+  # The lambda if true, then we keep working, else we failed and we recommend to do what we have on the left
+  # We only show the optional ones if subsequent steps failed. If the next mandatory step passed then we ignore
+  # the options
   def lesson_status_en(lesson_name)
     dropbox_dir = DropboxDir.new(DROPBOX_ROOT, :en)
     stages = {'script' => ->{ScriptsDir.new.done?(lesson_name + '_en.md')},
       'record audio' => ->{(dropbox_dir.has_audio?(lesson_name) && !dropbox_dir.has_video?(lesson_name)) || :option},
       'record video' => ->{(dropbox_dir.has_video?(lesson_name) && !dropbox_dir.has_audio?(lesson_name)) || :option},
-      'camtasia' => ->{(dropbox_dir.has_audio?(lesson_name) && dropbox_dir.has_video?(lesson_name)) || :option},
-      'publish video' => ->{dropbox_dir.has_project?(lesson_name)},
-     # 'publish site' => ->{VideoManager.new.video_published?(lesson_name, :en)} TODO
-      'DONE' => ->{Site.new.published?(lesson_name, :en)}
+    #  'camtasia' => ->{(dropbox_dir.has_audio?(lesson_name) && dropbox_dir.has_video?(lesson_name)) || :option},
+      'camtasia' => ->{dropbox_dir.has_project?(lesson_name)},
+      'publish video' => ->{VideoManager.new.video_published?(lesson_name, :en)},
+      'publish_site' => ->{Site.new.published?(lesson_name, :en)}
     }
 
-      next_step = next_action(stages)
-      if next_step == 'script'
-       ScriptsDir.new.status?(lesson_name + '_en.md')
-      else
-        next_step
-      end
-
+    next_step = next_action(stages)
+    if next_step == 'script'
+     ScriptsDir.new.status?(lesson_name + '_en.md')
+    else
+      next_step
+    end
   end
 
   def lesson_status_es(lesson_name)
@@ -59,7 +63,7 @@ class LessonStatus
       'wait es video' => ->{dropbox_dir.has_translated_video?(lesson_name) || :option},
       'review video' => ->{dropbox_dir.has_finished_video?(lesson_name) || :option},
       'publish video' => ->{ VideoManager.new.video_published?(lesson_name, :es)},
-      'DONE' => ->{Site.new.published?(lesson_name, :es)}
+      'publish site' => ->{Site.new.published?(lesson_name, :es)}
    # 'DONE' => ->{VideoManager.new.video_published?(lesson_name, :es)} #TODO publish_site
     }
 
--- lib/snow/site.rb
+++ lib/snow/site.rb
@@ -1,12 +1,13 @@
 class Site
   def published?(lesson_name, language)
-    return false
+    return true
     # TODO: implement this
     lesson_title = ScriptsDir.new.lesson_title(lesson_name+ '_en.md')
     puts lesson_title_to_url(lesson_title, language)
     uri = URI(lesson_title_to_url(lesson_title, language))
     #TODO: why we get 301 when using head?
     response = Net::HTTP.get_response(uri)
+    puts response.code
     response.code.to_i == 200
   end
 
@@ -20,7 +21,7 @@ class Site
     if language == :en
       'https://railsmania.com/en/lessons/'
     elsif language == :es
-      'https://railsmania.com/en/lessons/'
+      'https://railsmania.com/es/lessons/'
     else
       raise 'language not supported'
     end
